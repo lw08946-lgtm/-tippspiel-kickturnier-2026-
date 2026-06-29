b353b4fe0889372f87224b747c1e9f2f3c0e7296
lw08946-lgtm <lw08946@gmail.com>
2026-06-29 09:56:32 +0200

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,1 +1,15 @@
+// Tippspiel Kickturnier 2026
 
+console.log("Tippspiel Kickturnier 2026 gestartet.");
+
+function login() {
+    const name = document.getElementById("name").value;
+    const code = document.getElementById("code").value;
+
+    if (name === "" || code === "") {
+        alert("Bitte Name und Zugangscode eingeben.");
+        return;
+    }
+
+    alert("Willkommen " + name + "!");
+}
